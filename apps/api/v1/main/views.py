6246a3fc2e50c8bf62f9f772db358498e2343d33
SaD-Pr0ger <sad.pr0ger@gmail.com>
2024-03-12 20:40:12 +0500

    urls structure/sample view

--- apps/api/v1/main/views.py
+++ apps/api/v1/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def example(request: HttpRequest) -> HttpResponse:
+    return Response({'status': 'OK'}, status=200)
