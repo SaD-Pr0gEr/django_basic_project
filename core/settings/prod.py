00d4b9a01cb0671d3096d31e74c1a05d53d129c2
Озод <86515876+SaD-Pr0gEr@users.noreply.github.com>
2024-04-04 21:44:32 +0500

    Update prod.py

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 DATABASES = {
     'default': {
@@ -19,4 +19,4 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
